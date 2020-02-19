2118f75225b0b121726f4a75f13932765feeffc7
iqbal-singh <isingh2@students.towson.edu>
2020-02-18 19:18:31 -0500

    Updates

---
+++ precache-manifest.60c033b55e315bab72773424133d734c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1646e0b81bd2c957d6cec905fedca21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69722d27904c92a883a2",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "be0484a5ca1e6413165b",
+    "url": "/static/js/2.137debc5.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.137debc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69722d27904c92a883a2",
+    "url": "/static/js/main.c15eca91.chunk.js"
+  },
+  {
+    "revision": "cd53667854ec47b088ca",
+    "url": "/static/js/runtime-main.6a98dd4a.js"
+  }
+]);
